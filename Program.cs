83e72db732bdb171e5addbb842c0d6c069cacd67
Ali Denno <aliedenno@gmail.com>
2016-07-11 09:24:41 +0200

    Init: The repo with the initial Code & README.md

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileNames_Fetcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Put all file names in root directory into array.
+            String Path = @"C:\Users\ali-d\Documents\Debian\myFiles\Ethical Hacking Buffer Overflow\01 What Are Buffer Overflows\";
+            Console.WriteLine(Path);
+            string[] array1 = Directory.GetFiles(Path);
+
+            // Put all bin files in root directory into array.
+            // ... This is case-insensitive.
+            string[] array2 = Directory.GetFiles(@"C:\", "*.BIN");
+
+            // Display all files.
+            Console.WriteLine("--- Files: ---");
+            foreach (string name in array1)
+            {
+                Console.WriteLine(name);
+            }
+
+            // Display all BIN files.
+            Console.WriteLine("--- BIN Files: ---");
+            foreach (string name in array2)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Basic program for personal use, it would fetch the names of different files located into a given folder 
+
+@under work
+Initialized @Monday, July 11, 2016
+
